0a0baf599b34b338fdc50a4a0bd41338cb384699
Gihan Karunarathne <gckarunarathne@gmail.com>
2017-04-21 01:40:34 +0530

    Implemented Extracting FLO2D OUTPUT into ErisGRID format

---
+++ FLO2DTOLEVELGRID.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python3
+
+import os, json, subprocess, datetime, sys
+from os import curdir
+from os.path import join as pjoin
+from sys import executable
+from subprocess import Popen
+
+from FLO2DWATERLEVEL import getWaterLevelGrid
+from FLO2DWATERLEVEL import getGridBoudary
+from FLO2DWATERLEVEL import getCellGrid
+from FLO2DWATERLEVEL import getEsriGrid
+
+try :
+    CONFIG = json.loads(open('CONFIG.json').read())
+
+    CWD = os.getcwd()
+    BASE_OUT_FILE = 'BASE.OUT'
+    WATER_LEVEL_FILE = 'water_level.asc'
+    WATER_LEVEL_DIR = 'water_level'
+    OUTPUT_DIR = 'OUTPUT'
+    if 'BASE_OUT_FILE' in CONFIG :
+        BASE_OUT_FILE = CONFIG['BASE_OUT_FILE']
+    if 'WATER_LEVEL_FILE' in CONFIG :
+        WATER_LEVEL_FILE = CONFIG['WATER_LEVEL_FILE']
+    if 'OUTPUT_DIR' in CONFIG :
+        OUTPUT_DIR = CONFIG['OUTPUT_DIR']
+
+    # Default run for current day
+    now = datetime.datetime.now()
+    if len(sys.argv) > 1 : # Or taken from first arg for the program
+        now = datetime.datetime.strptime(sys.argv[1], '%Y-%m-%d')
+    date = now.strftime("%Y-%m-%d")
+    print('Extract Result of FLO2D on', date)
+
+    appDir = pjoin(CWD, date + '_Kelani')
+    OUTPUT_DIR_PATH = pjoin(CWD, 'OUTPUT')
+    BASE_OUT_FILE_PATH = pjoin(appDir, BASE_OUT_FILE)
+    WATER_LEVEL_DIR_PATH = pjoin(OUTPUT_DIR_PATH, "%s-%s" % (WATER_LEVEL_DIR, date))
+
+    # Create OUTPUT Directory
+    if not os.path.exists(OUTPUT_DIR_PATH):
+        os.makedirs(OUTPUT_DIR_PATH)
+
+    bufsize = 65536
+    with open(BASE_OUT_FILE_PATH) as infile: 
+        i=0
+        isWaterLevelLines = False
+        waterLevelLines = []
+        while True:
+            lines = infile.readlines(bufsize)
+            i += 1
+            if i > 2 :
+                break
+            if not lines:
+                break
+            for line in lines:
+                if line.startswith('MODEL TIME =', 5) :
+                    isWaterLevelLines = True
+                elif isWaterLevelLines and line.startswith('***CHANNEL RESULTS***', 17) :
+                    waterLevels = getWaterLevelGrid(waterLevelLines)
+                    boundary    = getGridBoudary()
+                    CellGrid    = getCellGrid(boundary)
+                    EsriGrid    = getEsriGrid(waterLevels, boundary, CellGrid)
+
+                    # Create Directory
+                    if not os.path.exists(WATER_LEVEL_DIR_PATH):
+                        os.makedirs(WATER_LEVEL_DIR_PATH)
+                    # Get Time stamp Ref:http://stackoverflow.com/a/13685221/1461060
+                    ModelTime = float(waterLevelLines[0].split()[3])
+                    fileModelTime = datetime.datetime.strptime(date, '%Y-%m-%d')
+                    fileModelTime = fileModelTime + datetime.timedelta(hours=ModelTime)
+                    dateAndTime = fileModelTime.strftime("%Y-%m-%d_%H:%M:%S")
+                    # Create files
+                    fileName = WATER_LEVEL_FILE.split('.', 1)
+                    fileName = "%s-%s.%s" % (fileName[0], dateAndTime, fileName[1])
+                    WATER_LEVEL_FILE_PATH = "%s/%s" % (WATER_LEVEL_DIR_PATH, fileName)
+                    file = open(WATER_LEVEL_FILE_PATH, 'w')
+                    file.writelines(EsriGrid)
+
+                    isWaterLevelLines = False
+                    # for l in waterLevelLines :
+                        # print(l)
+                    waterLevelLines = []
+
+                if isWaterLevelLines :
+                    waterLevelLines.append(line)
+
+except Exception as e :
+    traceback.print_exc()
+finally:
+    file.close()
+    print('Completed processing', BASE_OUT_FILE_PATH, ' to ', WATER_LEVEL_FILE)
---
+++ FLO2DWATERLEVEL.py
@@ -0,0 +1,118 @@
+#!/usr/bin/python3
+
+import os, json, datetime, sys, math
+from os.path import join as pjoin
+
+SKIP_META_LINES = 6
+# FLO2D cells to longitude, latitude mapping file location
+CADPTS_DAT_FILE = 'FLO2d/CADPTS.DAT'
+CWD = os.getcwd()
+CADPTS_DAT_FILE_PATH = pjoin(CWD, CADPTS_DAT_FILE)
+
+def getWaterLevelGrid(lines) :
+    waterLevels = []
+    for line in lines[6:] :
+        if line == '\n' :
+            break
+        v = line.split()
+        waterLevels.append('%s %s' % (v[0], v[1]))
+
+    return waterLevels
+
+
+def getGridBoudary(gap=250.0) :
+    "longitude  -> x : larger value" 
+    "latitude   -> y : smaller value"
+
+    long_min = 1000000000.0
+    lat_min = 1000000000.0
+    long_max = 0.0
+    lat_max = 0.0
+
+    with open(CADPTS_DAT_FILE_PATH) as f:
+        lines = f.readlines()
+        for line in lines :
+            values = line.split()
+            long_min = min(long_min, float(values[1]))
+            lat_min = min(lat_min, float(values[2]))
+
+            long_max = max(long_max, float(values[1]))
+            lat_max = max(lat_max, float(values[2]))
+
+    return {
+        'long_min': long_min,
+        'lat_min': lat_min,
+        'long_max': long_max,
+        'lat_max': lat_max
+    }
+
+
+def getCellGrid(boudary, gap=250.0) :
+    CellMap = {}
+
+    with open(CADPTS_DAT_FILE_PATH) as f:
+        lines = f.readlines()
+        for line in lines :
+            v = line.split()
+            i = int((float(v[1]) - boudary['long_min']) / gap)
+            j = int((float(v[2]) - boudary['lat_min']) / gap)
+            if i < 0 or j < 0 :
+            CellMap[int(v[0])] = (i, j)
+
+    return CellMap
+
+
+def getEsriGrid(waterLevels, boudary, CellMap, gap=250.0, missingVal=-9999) :
+    "Esri GRID format : https://en.wikipedia.org/wiki/Esri_grid"
+    "ncols         4"
+    "nrows         6"
+    "xllcorner     0.0"
+    "yllcorner     0.0"
+    "cellsize      50.0"
+    "NODATA_value  -9999"
+    "-9999 -9999 5 2"
+    "-9999 20 100 36"
+    "3 8 35 10"
+    "32 42 50 6"
+    "88 75 27 9"
+    "13 5 1 -9999"
+
+    EsriGrid = []
+
+    cols = int(math.ceil((boudary['long_max'] - boudary['long_min']) / gap))
+    rows = int(math.ceil((boudary['lat_max'] - boudary['lat_min']) / gap))
+
+    Grid = [[-9999 for x in range(cols)] for y in range(rows)]
+
+    for level in waterLevels :
+        v = level.split()
+        i, j = CellMap[int(v[0])]
+        Grid[i][j] = float(v[1])
+
+    EsriGrid.append('%s\t%s\n' % ('ncols', cols))
+    EsriGrid.append('%s\t%s\n' % ('nrows', rows))
+    EsriGrid.append('%s\t%s\n' % ('xllcorner', boudary['long_min']))
+    EsriGrid.append('%s\t%s\n' % ('yllcorner', boudary['lat_min']))
+    EsriGrid.append('%s\t%s\n' % ('cellsize', gap))
+    EsriGrid.append('%s\t%s\n' % ('NODATA_value', missingVal))
+
+    for i in range(0, rows) :
+        arr = []
+        for j in range(0, cols) :
+            arr.append(Grid[i][j])
+
+        EsriGrid.append('%s\n' % (' '.join(str(x) for x in arr)))
+
+    return EsriGrid
+
+
+
+
+
+
+
+
+
+
+
+
