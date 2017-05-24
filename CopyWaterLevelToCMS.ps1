55d7aaeecaf59c43bcdfb6fb0227ea474329a2c0
Gihan Karunarathne <gckarunarathne@gmail.com>
2017-05-24 23:08:48 +0530

    Fixed: Minor, script not found

--- CopyWaterLevelToCMS.ps1
+++ CopyWaterLevelToCMS.ps1
@@ -6,6 +6,6 @@ if(!$d) {
 
 echo "forecast date : $d"
 
-python FLO2DTOLEVEL.py $d
+python FLO2DTOWATERLEVEL.py $d
 
 C:\udp\pscp.exe -i .\ssh\id_lahikos -r  .\OUTPUT\water_level-$d uwcc-admin@10.138.0.6:/home/uwcc-admin/cfcwm/data/FLO2D/WL
