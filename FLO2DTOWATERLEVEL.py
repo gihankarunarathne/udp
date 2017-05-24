de11b9bf0944e11cd844ce6b47ac26583ac111df
Gihan Karunarathne <gckarunarathne@gmail.com>
2017-05-24 22:23:33 +0530

    Extracted diff locations & write into diff files

--- FLO2DTOWATERLEVEL.py
+++ FLO2DTOWATERLEVEL.py
@@ -21,11 +21,37 @@ try :
     if 'OUTPUT_DIR' in CONFIG :
         OUTPUT_DIR = CONFIG['OUTPUT_DIR']
 
-    ELEMENT_NUMBERS = [
-        583,
-        514
-    ]
+    CELL_MAP = {
+        3627 : "Ambathale",
+        3626 : "Madiwela Out",
+        2438 : "Balalihini Out",
+        2439 : "Balalihini Out 2",
+        1365 : "Kittampahuwa Bridge",
+        1102 : "Kittampahuwa Out",
+        1103 : "Kittampahuwa Out 2",
+        645  : "N Street",
+        720  : "N Street 2",
+        713  : "Kolonnawa CNL 1",
+        1089 : "Kolonnawa CNL 2",
+        1458 : "Kolonnawa CNL 3",
+        1664 : "Kolonnawa CNL 4",
+        1879 : "Parliament Lake Out",
+        2308 : "Parliament Lake",
+        2305 : "Parliament Lake 2",
+        2712 : "Parliament Upstream",
+        2516 : "Ahangama",
+        3603 : "Madiwela US",
+        1000 : "Heen Ela",
+        621  : "Torington",
+        912  : "Wellawatta 1",
+        763  : "Wellawatta 2",
+        195  : "Wellawatta 3",
+        470  : "Dehiwala 1",
+        238  : "Dehiwala 2",
+    }
+    ELEMENT_NUMBERS = CELL_MAP.keys()
     SERIES_LENGTH = 144
+    MISSING_VALUE = -999
 
     # Default run for current day
     now = datetime.datetime.now()
@@ -66,6 +92,7 @@ try :
                     if elementNo in ELEMENT_NUMBERS :
                         print('>>>', elementNo)
                         isWaterLevelLines = True
+                        waterLevelLines.append(line)
                     else :
                         isWaterLevelLines = False
 
@@ -85,7 +112,8 @@ try :
 
                     baseTime = datetime.datetime.strptime(date, '%Y-%m-%d')
                     timeseries = []
-                    for ts in waterLevelLines :
+                    elementNo = int(waterLevelLines[0].split()[5])
+                    for ts in waterLevelLines[1:] :
                         v = ts.split()
                         if len(v) < 1 :
                             continue
@@ -93,6 +121,8 @@ try :
                         # value = v[1]
                         # Get flood depth (Depth)
                         value = v[2]
+                        if not value.replace('.','',1).isdigit() :
+                            value = MISSING_VALUE
                         timeStep = float(v[0])
                         currentStepTime = baseTime + datetime.timedelta(hours=timeStep)
                         dateAndTime = currentStepTime.strftime("%Y-%m-%d %H:%M:%S")
@@ -102,14 +132,13 @@ try :
                     if not os.path.exists(WATER_LEVEL_DIR_PATH):
                         os.makedirs(WATER_LEVEL_DIR_PATH)
                     # Get Time stamp Ref:http://stackoverflow.com/a/13685221/1461060
-                    ModelTime = float(waterLevelLines[0].split()[3])
-                    print('Model Time', ModelTime)
+                    ModelTime = float(waterLevelLines[1].split()[3])
                     fileModelTime = datetime.datetime.strptime(date, '%Y-%m-%d')
                     fileModelTime = fileModelTime + datetime.timedelta(hours=ModelTime)
                     dateAndTime = fileModelTime.strftime("%Y-%m-%d_%H-%M-%S")
                     # Create files
                     fileName = WATER_LEVEL_FILE.split('.', 1)
-                    fileName = "%s-%s.%s" % (fileName[0], date, fileName[1])
+                    fileName = "%s-%s-%s.%s" % (fileName[0], CELL_MAP[elementNo].replace(' ', '_'), date, fileName[1])
                     WATER_LEVEL_FILE_PATH = pjoin(WATER_LEVEL_DIR_PATH, fileName)
                     csvWriter = csv.writer(open(WATER_LEVEL_FILE_PATH, 'w'), delimiter=',', quotechar='|')
                     csvWriter.writerows(timeseries)
