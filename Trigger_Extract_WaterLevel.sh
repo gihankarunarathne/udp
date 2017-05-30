c289dde55c8a26a0d4f355181af36a3cfb4f4fb8
Gihan Karunarathne <gckarunarathne@gmail.com>
2017-05-30 14:21:57 +0530

    Added passing FLO2D model path

--- Trigger_Extract_WaterLevel.sh
+++ Trigger_Extract_WaterLevel.sh
@@ -13,6 +13,7 @@ Usage: ./Forecast.sh [-d FORECAST_DATE] [-c CONFIG_FILE] [-r ROOT_DIR] [-b DAYS_
 	-c 	Location of CONFIG.json. Default is Forecast.sh exist directory.
 	-b 	Run forecast specified DAYS_BACK with respect to current date. Expect an integer.
 		When specified -d option will be ignored.
+    -p  Path of FLO2D folder
 EOF
 }
 
@@ -24,9 +25,12 @@ trimQuotes() {
 
 forecast_date="`date +%Y-%m-%d`";
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+META_FLO2D_DIR=$ROOT_DIR/META_FLO2D
+FLO2D_DIR=$ROOT_DIR/FLO2D
 INIT_DIR=$(pwd)
 CONFIG_FILE=$ROOT_DIR/CONFIG.json
 DAYS_BACK=0
+FLO2D_PATH=""
 # Extract user arguments
 while getopts hd:c:b: opt; do
     case $opt in
@@ -40,6 +44,8 @@ while getopts hd:c:b: opt; do
             ;;
 		b)  DAYS_BACK=$OPTARG
 			;;
+        p)  FLO2D_PATH=$OPTARG
+            ;;
         *)
             usage >&2
             exit 1
@@ -70,6 +76,9 @@ current_date_time="`date +%Y-%m-%dT%H:%M:%S`";
 
 main() {
     echo "Start at $current_date_time"
+    cp $META_FLO2D_DIR/RUN_FLO2D.json $FLO2D_DIR
+    sed -i '/FLO2D_PATH/c\FLO2D_PATH : $FLO2D_PATH' $FLO2D_DIR/RUN_FLO2D.json
+
     echo "Trigger FLO2D WaterLevel Extraction on Forecast Date: $forecast_date, Config File: $CONFIG_FILE, Root Dir: $ROOT_DIR"
 
     curl -X POST --data-binary @./FLO2D/RUN_FLO2D.json  $WINDOWS_HOST/EXTRACT_WATERLEVEL?$forecast_date
