b543e6696b0b3cd00b73d2454b0ec88532b8f14b
Gihan Karunarathne <gckarunarathne@gmail.com>
2017-07-07 17:16:41 +0530

    Changed indentation from tabs to 4 spaces

--- Forecast.sh
+++ Forecast.sh
@@ -2,44 +2,44 @@
 
 #
 # ./Forecast.sh -d <FORECAST_DATE>
-#	e.g. ./Forecast.sh -d 2017-03-22
+#   e.g. ./Forecast.sh -d 2017-03-22
 #
 usage() {
 cat <<EOF
 Usage: ./Forecast.sh [-d FORECAST_DATE] [-t FORECAST_TIME] [-c CONFIG_FILE] [-r ROOT_DIR] [-b DAYS_BACK] [-f]
 
-	-h 		Show usage
-	-d 		Date which need to run the forecast in YYYY-MM-DD format. Default is current date.
-	-t 		Time which need to run the forecast in HH:MM:SS format. Default is current hour. Run on hour resolution only.
-	-c 		Location of CONFIG.json. Default is Forecast.sh exist directory.
-	-r 		ROOT_DIR which is program running directory. Default is Forecast.sh exist directory.
-	-b 		Run forecast specified DAYS_BACK with respect to current date. Expect an integer.
-			When specified -d option will be ignored.
-	-f 		Force run forecast. Even the forecast already run for the particular day, run again. Default is false.
-	-i 		Initiate a State at the end of HEC-HMS run.
-	-s 		Store Timeseries data on MySQL database.
-	-e  	Exit without executing models which run on Windows.
-	-C  	(Control Interval in minutes) Time period that HEC-HMS model should run
+    -h      Show usage
+    -d      Date which need to run the forecast in YYYY-MM-DD format. Default is current date.
+    -t      Time which need to run the forecast in HH:MM:SS format. Default is current hour. Run on hour resolution only.
+    -c      Location of CONFIG.json. Default is Forecast.sh exist directory.
+    -r      ROOT_DIR which is program running directory. Default is Forecast.sh exist directory.
+    -b      Run forecast specified DAYS_BACK with respect to current date. Expect an integer.
+            When specified -d option will be ignored.
+    -f      Force run forecast. Even the forecast already run for the particular day, run again. Default is false.
+    -i      Initiate a State at the end of HEC-HMS run.
+    -s      Store Timeseries data on MySQL database.
+    -e      Exit without executing models which run on Windows.
+    -C      (Control Interval in minutes) Time period that HEC-HMS model should run
 
-	-T|--tag 	Tag to differential simultaneous Forecast Runs E.g. wrf1, wrf2 ...
+    -T|--tag    Tag to differential simultaneous Forecast Runs E.g. wrf1, wrf2 ...
 
-	--wrf-out		Path of WRF_OUTPUT directory. If this is set, then 
-						<WRF_OUT>/RF 					(<-RF_DIR_PATH)
-						<WRF_OUT>/kelani-upper-basin	(<-)
-						<WRF_OUT>/colombo
-						<WRF_OUT>/kelani-basin will use respectively instead of CONFIG.json.
-					Otherwise using the values from CONFIG.json
-	--wrf-rf 		
-	--wrf-kub
-	--wrf-rf-grid
-	--wrf-raincell
+    --wrf-out       Path of WRF_OUTPUT directory. If this is set, then 
+                        <WRF_OUT>/RF                    (<-RF_DIR_PATH)
+                        <WRF_OUT>/kelani-upper-basin    (<-)
+                        <WRF_OUT>/colombo
+                        <WRF_OUT>/kelani-basin will use respectively instead of CONFIG.json.
+                    Otherwise using the values from CONFIG.json
+    --wrf-rf        
+    --wrf-kub
+    --wrf-rf-grid
+    --wrf-raincell
 EOF
 }
 
 trimQuotes() {
-	tmp="${1%\"}"
-	tmp="${tmp#\"}"
-	echo $tmp
+    tmp="${1%\"}"
+    tmp="${tmp#\"}"
+    echo $tmp
 }
 
 forecast_date="`date +%Y-%m-%d`";
@@ -59,8 +59,8 @@ WRF_OUT=""
 # Read the options
 # Ref: http://www.bahmanm.com/blogs/command-line-options-how-to-parse-in-bash-using-getopt
 TEMP=`getopt -o hd:t:c:r:b:fiseC:T: \
-		--long arga::,argb,argc:,tag:,wrf-out: \
-		-n 'Forecast.sh' -- "$@"`
+        --long arga::,argb,argc:,tag:,wrf-out: \
+        -n 'Forecast.sh' -- "$@"`
 
 # Terminate on wrong args. Ref: https://stackoverflow.com/a/7948533/1461060
 if [ $? != 0 ] ; then usage >&2 ; exit 1 ; fi
@@ -102,31 +102,31 @@ while true ; do
                 *) CONFIG_FILE="$2" ; shift 2 ;;
             esac ;;
         -r)
-			case "$2" in
+            case "$2" in
                 "") shift 2 ;;
                 *) ROOT_DIR="$2" ; shift 2 ;;
             esac ;;
-		-b)
-			case "$2" in
+        -b)
+            case "$2" in
                 "") shift 2 ;;
                 *) DAYS_BACK="$2" ; shift 2 ;;
             esac ;;
-		-f)  FORCE_RUN=true ; shift ;;
-		-i)  INIT_STATE=true ; shift ;;
-		-s)  STORE_DATA=true ; shift ;;
-		-e)  FORCE_EXIT=true ; shift ;;
-		-C)
-			case "$2" in
+        -f)  FORCE_RUN=true ; shift ;;
+        -i)  INIT_STATE=true ; shift ;;
+        -s)  STORE_DATA=true ; shift ;;
+        -e)  FORCE_EXIT=true ; shift ;;
+        -C)
+            case "$2" in
                 "") shift 2 ;;
                 *) CONTROL_INTERVAL="$2" ; shift 2 ;;
             esac ;;
         -T|--tag)
-			case "$2" in
+            case "$2" in
                 "") shift 2 ;;
                 *) TAG="$2" ; shift 2 ;;
             esac ;;
         --wrf-out)
-			case "$2" in
+            case "$2" in
                 "") shift 2 ;;
                 *) WRF_OUT="$2" ; shift 2 ;;
             esac ;;
@@ -137,16 +137,16 @@ while true ; do
 done
 
 if [ "$DAYS_BACK" -gt 0 ]; then
-	#TODO: Try to back date base on user given date
-	forecast_date="`date +%Y-%m-%d -d "$DAYS_BACK days ago"`";
+    #TODO: Try to back date base on user given date
+    forecast_date="`date +%Y-%m-%d -d "$DAYS_BACK days ago"`";
 fi
 
 # cd into bash script's root directory
 cd $ROOT_DIR
 echo "Current Working Directory set to -> $(pwd)"
 if [ -z "$(find $CONFIG_FILE -name CONFIG.json)" ]; then
-	echo "Unable to find $CONFIG_FILE file"
-	exit 1
+    echo "Unable to find $CONFIG_FILE file"
+    exit 1
 fi
 
 HOST_ADDRESS=$(trimQuotes $(cat CONFIG.json | jq '.HOST_ADDRESS'))
@@ -167,117 +167,117 @@ DSS_OUTPUT_FILE=$(trimQuotes $(cat CONFIG.json | jq '.DSS_OUTPUT_FILE'))
 current_date_time="`date +%Y-%m-%dT%H:%M:%S`";
 
 main() {
-	if [[ "$TAG" =~ [^a-zA-Z0-9\ ] ]]; then
-		echo "Parameter for -T|--tag is \"$TAG\" invalid. It can onaly contain alphanumberic values."
-		exit 1;
-	fi
+    if [[ "$TAG" =~ [^a-zA-Z0-9\ ] ]]; then
+        echo "Parameter for -T|--tag is \"$TAG\" invalid. It can onaly contain alphanumberic values."
+        exit 1;
+    fi
 
-	if [ ! -z $WRF_OUT ] && [ -d $WRF_OUT ]; then
-		RF_DIR_PATH=$WRF_OUT/RF
-		KUB_DIR_PATH=$WRF_OUT/kelani-upper-basin
-		RF_GRID_DIR_PATH=$WRF_OUT/colombo
-		FLO2D_RAINCELL_DIR_PATH=$WRF_OUT/kelani-basin
-		echo "WRF OUT paths changed to -> $RF_DIR_PATH, $KUB_DIR_PATH, $RF_GRID_DIR_PATH, $FLO2D_RAINCELL_DIR_PATH"
-	fi
+    if [ ! -z $WRF_OUT ] && [ -d $WRF_OUT ]; then
+        RF_DIR_PATH=$WRF_OUT/RF
+        KUB_DIR_PATH=$WRF_OUT/kelani-upper-basin
+        RF_GRID_DIR_PATH=$WRF_OUT/colombo
+        FLO2D_RAINCELL_DIR_PATH=$WRF_OUT/kelani-basin
+        echo "WRF OUT paths changed to -> $RF_DIR_PATH, $KUB_DIR_PATH, $RF_GRID_DIR_PATH, $FLO2D_RAINCELL_DIR_PATH"
+    fi
 
-	echo "Start at $current_date_time $FORCE_EXIT"
-	echo "Forecasting with Forecast Date: $forecast_date @ $forecast_time, Config File: $CONFIG_FILE, Root Dir: $ROOT_DIR"
+    echo "Start at $current_date_time $FORCE_EXIT"
+    echo "Forecasting with Forecast Date: $forecast_date @ $forecast_time, Config File: $CONFIG_FILE, Root Dir: $ROOT_DIR"
 
-	local isWRF=$(isWRFAvailable)
-	local forecastStatus=$(alreadyForecast $ROOT_DIR/$STATUS_FILE $forecast_date)
-	if [ $FORCE_RUN == true ]; then
-		forecastStatus=0
-	fi
-	echo "isWRF $isWRF forecastStatus $forecastStatus"
+    local isWRF=$(isWRFAvailable)
+    local forecastStatus=$(alreadyForecast $ROOT_DIR/$STATUS_FILE $forecast_date)
+    if [ $FORCE_RUN == true ]; then
+        forecastStatus=0
+    fi
+    echo "isWRF $isWRF forecastStatus $forecastStatus"
 
-	if [ $isWRF == 1 ] && [ $forecastStatus == 0 ]; then
-		mkdir $OUTPUT_DIR
-		
-		# Read WRF forecast data, then create precipitation .csv for Upper Catchment 
-		# using Theissen Polygen
-		./RFTOCSV.py -d $forecast_date -t $forecast_time
+    if [ $isWRF == 1 ] && [ $forecastStatus == 0 ]; then
+        mkdir $OUTPUT_DIR
+        
+        # Read WRF forecast data, then create precipitation .csv for Upper Catchment 
+        # using Theissen Polygen
+        ./RFTOCSV.py -d $forecast_date -t $forecast_time
 
-		# HACK: There is an issue with running HEC-HMS model, it gave a sudden value change after 1 day
-		# We discovered that, this issue on 3.5 version, hence upgrade into 4.1
-		# But with 4.1, it runs correctly when the data are saved by the HEC-HMS program
-		# After run the model using the script, it can't reuse for a correct run again
-		# Here we reuse a corrected model which can run using the script
-		yes | cp -R 2008_2_Events_Hack/* 2008_2_Events/
+        # HACK: There is an issue with running HEC-HMS model, it gave a sudden value change after 1 day
+        # We discovered that, this issue on 3.5 version, hence upgrade into 4.1
+        # But with 4.1, it runs correctly when the data are saved by the HEC-HMS program
+        # After run the model using the script, it can't reuse for a correct run again
+        # Here we reuse a corrected model which can run using the script
+        yes | cp -R 2008_2_Events_Hack/* 2008_2_Events/
 
-		# Remove .dss files in order to remove previous results
-		rm $DSS_INPUT_FILE
-		rm $DSS_OUTPUT_FILE
-		# Read Avg precipitation, then create .dss input file for HEC-HMS model
-		./dssvue/hec-dssvue.sh CSVTODSS.py --date $forecast_date
+        # Remove .dss files in order to remove previous results
+        rm $DSS_INPUT_FILE
+        rm $DSS_OUTPUT_FILE
+        # Read Avg precipitation, then create .dss input file for HEC-HMS model
+        ./dssvue/hec-dssvue.sh CSVTODSS.py --date $forecast_date
 
-		# Change HEC-HMS running time window
-		./Update_HECHMS.py -d $forecast_date \
-			`[[ $INIT_STATE == true ]] && echo "-i" || echo ""` \
-			`[[ $CONTROL_INTERVAL == 0 ]] && echo "" || echo "-c $CONTROL_INTERVAL"`
+        # Change HEC-HMS running time window
+        ./Update_HECHMS.py -d $forecast_date \
+            `[[ $INIT_STATE == true ]] && echo "-i" || echo ""` \
+            `[[ $CONTROL_INTERVAL == 0 ]] && echo "" || echo "-c $CONTROL_INTERVAL"`
 
-		# Run HEC-HMS model
-		cd $ROOT_DIR/$HEC_HMS_DIR
-		./HEC-HMS.sh -s ../2008_2_Events/2008_2_Events.script
-		cd $ROOT_DIR
+        # Run HEC-HMS model
+        cd $ROOT_DIR/$HEC_HMS_DIR
+        ./HEC-HMS.sh -s ../2008_2_Events/2008_2_Events.script
+        cd $ROOT_DIR
 
-		# Read HEC-HMS result, then extract Discharge into .csv
-		./dssvue/hec-dssvue.sh DSSTOCSV.py --date $forecast_date
+        # Read HEC-HMS result, then extract Discharge into .csv
+        ./dssvue/hec-dssvue.sh DSSTOCSV.py --date $forecast_date
 
-		# Read Discharge .csv, then create INFLOW.DAT file for FLO2D
-		./CSVTODAT.py  -d $forecast_date
+        # Read Discharge .csv, then create INFLOW.DAT file for FLO2D
+        ./CSVTODAT.py  -d $forecast_date
 
-		if [ $FORCE_EXIT == false ]; then
-			# Send INFLOW.DAT file into Windows
-			echo "Send POST request to $WINDOWS_HOST with INFLOW.DAT"
-			curl -X POST --data-binary @./FLO2D/INFLOW.DAT  $WINDOWS_HOST/INFLOW.DAT?$forecast_date
+        if [ $FORCE_EXIT == false ]; then
+            # Send INFLOW.DAT file into Windows
+            echo "Send POST request to $WINDOWS_HOST with INFLOW.DAT"
+            curl -X POST --data-binary @./FLO2D/INFLOW.DAT  $WINDOWS_HOST/INFLOW.DAT?$forecast_date
 
-			# Send RAINCELL.DAT file into Windows
-			echo "Send POST request to $WINDOWS_HOST with RAINCELL.DAT"
-			FLO2D_RAINCELL_FILE_PATH=$FLO2D_RAINCELL_DIR_PATH/created-$forecast_date/RAINCELL.DAT
-			curl -X POST --data-binary @$FLO2D_RAINCELL_FILE_PATH  $WINDOWS_HOST/RAINCELL.DAT?$forecast_date
+            # Send RAINCELL.DAT file into Windows
+            echo "Send POST request to $WINDOWS_HOST with RAINCELL.DAT"
+            FLO2D_RAINCELL_FILE_PATH=$FLO2D_RAINCELL_DIR_PATH/created-$forecast_date/RAINCELL.DAT
+            curl -X POST --data-binary @$FLO2D_RAINCELL_FILE_PATH  $WINDOWS_HOST/RAINCELL.DAT?$forecast_date
 
-			# Send RUN_FLO2D.json file into Windows, and run FLO2D
-			echo "Send POST request to $WINDOWS_HOST with RUN_FLO2D"
-			curl -X POST --data-binary @./FLO2D/RUN_FLO2D.json  $WINDOWS_HOST/RUN_FLO2D?$forecast_date
+            # Send RUN_FLO2D.json file into Windows, and run FLO2D
+            echo "Send POST request to $WINDOWS_HOST with RUN_FLO2D"
+            curl -X POST --data-binary @./FLO2D/RUN_FLO2D.json  $WINDOWS_HOST/RUN_FLO2D?$forecast_date
 
-			./CopyToCMS.sh -d $forecast_date
-		fi
-	
-		local writeStatus=$(alreadyForecast $ROOT_DIR/$STATUS_FILE $forecast_date)
-		if [ $writeStatus == 0 ]; then
-			writeForecastStatus $forecast_date $STATUS_FILE
-		fi
-	else
-		echo "WARN: Already run the forecast. Quit"
-		exit 1
-	fi
+            ./CopyToCMS.sh -d $forecast_date
+        fi
+    
+        local writeStatus=$(alreadyForecast $ROOT_DIR/$STATUS_FILE $forecast_date)
+        if [ $writeStatus == 0 ]; then
+            writeForecastStatus $forecast_date $STATUS_FILE
+        fi
+    else
+        echo "WARN: Already run the forecast. Quit"
+        exit 1
+    fi
 }
 
 isWRFAvailable() {
-	local File_Pattern="*$forecast_date*.txt"
-	if [ -z "$(find $RF_DIR_PATH -name $File_Pattern)" ]; then
-	  # echo "empty (Unable find files $File_Pattern)"
-	  echo 0
-	else
-	  # echo "Found WRF output"
-	  echo 1
-	fi
+    local File_Pattern="*$forecast_date*.txt"
+    if [ -z "$(find $RF_DIR_PATH -name $File_Pattern)" ]; then
+      # echo "empty (Unable find files $File_Pattern)"
+      echo 0
+    else
+      # echo "Found WRF output"
+      echo 1
+    fi
 }
 
 writeForecastStatus() {
-	echo $1 >> $2
+    echo $1 >> $2
 }
 
 alreadyForecast() {
-	local forecasted=0
+    local forecasted=0
 
-	while IFS='' read -r line || [[ -n "$line" ]]; do
-    	if [ $2 == $line ]; then
-    		forecasted=1
-    		break
-    	fi
-	done < "$1"
-	echo $forecasted
+    while IFS='' read -r line || [[ -n "$line" ]]; do
+        if [ $2 == $line ]; then
+            forecasted=1
+            break
+        fi
+    done < "$1"
+    echo $forecasted
 }
 
 main "$@"
