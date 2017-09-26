f73c621934896c4a03d5d1ec40bafb79828021ff
Gihan Karunarathne <gckarunarathne@gmail.com>
2017-09-26 14:59:05 +0530

    Implemented Running Hourly and Waiting time

--- scripts/ScriptRepeator.py
+++ scripts/ScriptRepeator.py
@@ -1,31 +1,40 @@
 #!/usr/bin/python3
 
-import sys, datetime, subprocess, argparse
+import sys, datetime, subprocess, argparse, time
 from subprocess import Popen
 
 try :
     parser = argparse.ArgumentParser()
-    parser.add_argument("-p", "--file-path", help="File name to be executed.")
-    parser.add_argument("-s", "--start-date", help="Start Date in YYYY-MM.")
-    parser.add_argument("-e", "--end-date", help="End Date in YYYY-MM.")
+    parser.add_argument("-p", "--file-path", help="File name to be executed.", required=True)
+    parser.add_argument("-s", "--start-date", help="Start Date in YYYY-MM.", required=True)
+    parser.add_argument("-e", "--end-date", help="End Date in YYYY-MM.", required=True)
     parser.add_argument("-f", "--force", action='store_true', help="Force insert.")
-    parser.add_argument("-v", "--version", help="Python version. eg: python3")
+    parser.add_argument("--exec", help="Executor that going to run the file.script. Default `python`. E.g: python3")
+    parser.add_argument("-i", "--interval", help="Time Interval between two events in hours")
+    parser.add_argument("-w", "--wait", help="Wait time for complete the task before run for next event in seconds")
     args = parser.parse_args()
     print('Commandline Options:', args)
+
+    timeInterval = 24
+    waitTime = 0
 
     if not args.file_path and args.start_date and args.end_date :
         print('All fields required.')
         sys.exit(2)
+    if args.interval :
+        timeInterval = int(args.interval)
+    if args.wait :
+        waitTime = int(args.wait)
 
     startDate = datetime.datetime.strptime(args.start_date, '%Y-%m-%d')
     endDate = datetime.datetime.strptime(args.end_date, '%Y-%m-%d')
 
-    pythonV = "python"
-    if args.version :
-        pythonV = args.version
+    executor = "python"
+    if args.exec :
+        executor = args.exec
 
     while(startDate <= endDate) :
-        execList = [pythonV, args.file_path]
+        execList = [executor, args.file_path]
         execList = execList + ['-d' , startDate.strftime("%Y-%m-%d")]
         if args.force :
             execList = execList + ['-f']
@@ -33,9 +42,10 @@ try :
         print('>>>', execList, '\n')
         proc = Popen(execList, stdout=sys.stdout)
         proc.wait()
+        time.sleep(waitTime)
         print('\n\n')
 
-        startDate = startDate + datetime.timedelta(days=1)
+        startDate = startDate + datetime.timedelta(hours=timeInterval)
 
 except ValueError:
     raise ValueError("Incorrect data format, should be YYYY-MM-DD")
