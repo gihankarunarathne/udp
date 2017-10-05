2f6abcf0171f2e3ed6c5355d8633667e1eccc4d4
Gihan Karunarathne <gckarunarathne@gmail.com>
2017-10-05 23:01:28 +0530

    Implemented passing run table name & using values in RUN_FLO2D.json
    If RUN_FLO2D.json file is reside in the FLO2D model dir, then use them
    as default values. But those values will be override by params passing to
    the script

--- EXTRACTFLO2DWATERLEVEL.py
+++ EXTRACTFLO2DWATERLEVEL.py
@@ -13,16 +13,19 @@ from LIBFLO2DWATERLEVELGRID import getWaterLevelOfChannels
 
 def usage() :
     usageText = """
-Usage: ./FLO2DTOWATERLEVEL.py [-d YYYY-MM-DD] [-t HH:MM:SS] [-p -o -h] [-S YYYY-MM-DD] [-T HH:MM:SS]
+Usage: ./EXTRACTFLO2DTOWATERLEVEL.py [-d YYYY-MM-DD] [-t HH:MM:SS] [-p -o -h] [-S YYYY-MM-DD] [-T HH:MM:SS]
 
 -h  --help          Show usage
--d  --date          Date in YYYY-MM-DD. Default is current date.
--t  --time          Time in HH:MM:SS. If -d passed, then default is 00:00:00. Otherwise Default is current time.
+-f #TODO
+-F  --flo2d_config  Configuration for FLO2D model run
+-d  --date          Model State Date in YYYY-MM-DD. Default is current date.
+-t  --time          Model State Time in HH:MM:SS. If -d passed, then default is 00:00:00. Otherwise Default is current time.
+-S  --start_date    Base Date of FLO2D model output in YYYY-MM-DD format. Default is same as -d option value.
+-T  --start_time    Base Time of FLO2D model output in HH:MM:SS format. Default is set to 00:00:00
 -p  --path          FLO2D model path which include HYCHAN.OUT
 -o  --out           Suffix for 'water_level-<SUFFIX>' and 'water_level_grid-<SUFFIX>' output directories.
                     Default is 'water_level-<YYYY-MM-DD>' and 'water_level_grid-<YYYY-MM-DD>' same as -d option value.
--S  --start_date    Base Date of FLO2D model output in YYYY-MM-DD format. Default is same as -d option value.
--T  --start_time    Base Time of FLO2D model output in HH:MM:SS format. Default is set to 00:00:00
+-n  --name          Name field value of the Run table in Database. Use time format such as <%H:%M:%S> to replace with time(t).
 """
     print(usageText)
 
@@ -41,12 +44,25 @@ def saveForecastTimeseries(adapter, timeseries, date, time, opts) :
     # for ll in extractedTimeseries :
     #     print(ll)
 
+    dateTime = datetime.datetime.strptime('%s %s' % (date, time), '%Y-%m-%d %H:%M:%S')
+    
     # Check whether existing station
     forceInsert = opts.get('forceInsert', False)
     station = opts.get('station', '')
-    # TODO: Check whether exist in Database
+    # TODO: Check whether station exist in Database
+    runName = opts.get('runName', 'Cloud-1')
+    lessCharIndex = runName.find('<')
+    greaterCharIndex = runName.find('>')
+    if lessCharIndex > -1 and greaterCharIndex > -1 and lessCharIndex < greaterCharIndex :
+        startStr = runName[:lessCharIndex]
+        dateFormatStr = runName[lessCharIndex+1:greaterCharIndex]
+        endStr = runName[greaterCharIndex+1:]
+        try:
+            dateStr = dateTime.strftime(dateFormatStr)
+            runName = startStr + dateStr + endStr
+        except ValueError:
+            raise ValueError("Incorrect data format " + dateFormatStr)
 
-    dateTime = datetime.datetime.strptime('%s %s' % (date, time), '%Y-%m-%d %H:%M:%S')
     types = [
         'Forecast-0-d',
         'Forecast-1-d-after',
@@ -70,7 +86,7 @@ def saveForecastTimeseries(adapter, timeseries, date, time, opts) :
         'unit': 'm',
         'type': types[0],
         'source': 'FLO2D',
-        'name': 'Cloud-1 ' + dateTime.strftime("%H:00:00"),
+        'name': runName
     }
     for i in range(0, min(len(types), len(extractedTimeseries))) :
         metaData['type'] = types[i]
@@ -99,6 +115,7 @@ try :
     WATER_LEVEL_FILE = 'water_level.txt'
     WATER_LEVEL_DIR = 'water_level'
     OUTPUT_DIR = 'OUTPUT'
+    RUN_FLO2D_FILE='RUN_FLO2D.json'
 
     MYSQL_HOST="localhost"
     MYSQL_USER="root"
@@ -157,17 +174,21 @@ try :
     output_suffix = ''
     start_date = ''
     start_time = ''
+    flo2d_config = ''
+    run_name = 'Cloud-1'
     forceInsert = False
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "hd:t:p:o:S:T:f", 
-            ["help", "date=", "time=", "path=", "out=", "start_date=", "start_time=", "forceInsert"])
+        opts, args = getopt.getopt(sys.argv[1:], "hF:d:t:p:o:S:T:fn:", 
+            ["help", "flo2d_config=", "date=", "time=", "path=", "out=", "start_date=", "start_time=", "name=", "forceInsert"])
     except getopt.GetoptError:          
         usage()                        
         sys.exit(2)                     
     for opt, arg in opts:
         if opt in ("-h", "--help"):
-            usage()                     
-            sys.exit()           
+            usage()  
+            sys.exit()
+        elif opt in ("-F", "--flo2d_config"):
+            flo2d_config = arg
         elif opt in ("-d", "--date"):
             date = arg
         elif opt in ("-t", "--time"):
@@ -180,14 +201,34 @@ try :
             start_date = arg.strip()
         elif opt in ("-T", "--start_time"):
             start_time = arg.strip()
+        elif opt in ("-n", "--name"):
+            run_name = arg.strip()
         elif opt in ("-f", "--forceInsert"):
             forceInsert = True
 
+    appDir = pjoin(CWD, date + '_Kelani')
+    if path :
+        appDir = pjoin(CWD, path)
+
+    # Load FLO2D Configuration file for the Model run if available
+    FLO2D_CONFIG_FILE = pjoin(appDir, RUN_FLO2D_FILE)
+    if flo2d_config :
+        FLO2D_CONFIG_FILE = pjoin(CWD, flo2d_config)
+    FLO2D_CONFIG = json.loads('{}')
+    # Check FLO2D Config file exists
+    if os.path.exists(FLO2D_CONFIG_FILE):
+        FLO2D_CONFIG = json.loads(open(FLO2D_CONFIG_FILE).read())
+
     # Default run for current day
     now = datetime.datetime.now()
+    if 'MODEL_STATE_DATE' in FLO2D_CONFIG : # Use FLO2D Config file data, if available
+        now = datetime.datetime.strptime(FLO2D_CONFIG['MODEL_STATE_DATE'], '%Y-%m-%d')
     if date :
         now = datetime.datetime.strptime(date, '%Y-%m-%d')
     date = now.strftime("%Y-%m-%d")
+
+    if 'MODEL_STATE_TIME' in FLO2D_CONFIG : # Use FLO2D Config file data, if available
+        now = datetime.datetime.strptime('%s %s' % (date, FLO2D_CONFIG['MODEL_STATE_TIME']), '%Y-%m-%d %H:%M:%S')
     if time :
         now = datetime.datetime.strptime('%s %s' % (date, time), '%Y-%m-%d %H:%M:%S')
     time = now.strftime("%H:%M:%S")
@@ -195,11 +236,17 @@ try :
     if start_date :
         start_date = datetime.datetime.strptime(start_date, '%Y-%m-%d')
         start_date = start_date.strftime("%Y-%m-%d")
+    else if 'TIMESERIES_START_DATE' in FLO2D_CONFIG : # Use FLO2D Config file data, if available
+        start_date = datetime.datetime.strptime(FLO2D_CONFIG['TIMESERIES_START_DATE']), '%Y-%m-%d')
+        start_date = start_date.strftime("%Y-%m-%d")
     else :
         start_date = date
 
     if start_time :
         start_time = datetime.datetime.strptime('%s %s' % (start_date, start_time), '%Y-%m-%d %H:%M:%S')
+        start_time = start_time.strftime("%H:%M:%S")
+    else if 'TIMESERIES_START_TIME' in FLO2D_CONFIG : # Use FLO2D Config file data, if available
+        start_time = datetime.datetime.strptime('%s %s' % (start_date, FLO2D_CONFIG['TIMESERIES_START_DATE']), '%Y-%m-%d %H:%M:%S')
         start_time = start_time.strftime("%H:%M:%S")
     else :
         start_time = datetime.datetime.strptime(start_date, '%Y-%m-%d') # Time is set to 00:00:00
@@ -207,14 +254,12 @@ try :
 
     print('Extract Water Level Result of FLO2D on', date, '@', time, 'with Bast time of', start_date, '@', start_time)
 
-    appDir = pjoin(CWD, date + '_Kelani')
-    if path :
-        appDir = pjoin(CWD, path)
-
     OUTPUT_DIR_PATH = pjoin(CWD, OUTPUT_DIR)
     HYCHAN_OUT_FILE_PATH = pjoin(appDir, HYCHAN_OUT_FILE)
 
     WATER_LEVEL_DIR_PATH = pjoin(OUTPUT_DIR_PATH, "%s-%s" % (WATER_LEVEL_DIR, date))
+    if 'FLO2D_OUTPUT_SUFFIX' in FLO2D_CONFIG : # Use FLO2D Config file data, if available
+        WATER_LEVEL_DIR_PATH = pjoin(OUTPUT_DIR_PATH, "%s-%s" % (WATER_LEVEL_DIR, FLO2D_CONFIG['FLO2D_OUTPUT_SUFFIX']))
     if output_suffix :
         WATER_LEVEL_DIR_PATH = pjoin(OUTPUT_DIR_PATH, "%s-%s" % (WATER_LEVEL_DIR, output_suffix))
 
@@ -231,7 +276,7 @@ try :
 
     # Calculate the size of time series
     bufsize = 65536
-    with open(HYCHAN_OUT_FILE_PATH) as infile: 
+    with open(HYCHAN_OUT_FILE_PATH) as infile:
         isWaterLevelLines = False
         isCounting = False
         countSeriesSize = 0 # HACK: When it comes to the end of file, unable to detect end of time series
@@ -327,7 +372,8 @@ try :
                     # Save Forecast values into Database
                     opts = {
                         'forceInsert': forceInsert,
-                        'station': CHANNEL_CELL_MAP[elementNo]
+                        'station': CHANNEL_CELL_MAP[elementNo],
+                        'runName': run_name
                     }
                     adapter = mysqladapter(host=MYSQL_HOST, user=MYSQL_USER, password=MYSQL_PASSWORD, db=MYSQL_DB)
                     saveForecastTimeseries(adapter, timeseries, date, time, opts)
@@ -397,7 +443,8 @@ try :
             # Save Forecast values into Database
             opts = {
                 'forceInsert': forceInsert,
-                'station': FLOOD_PLAIN_CELL_MAP[elementNo]
+                'station': FLOOD_PLAIN_CELL_MAP[elementNo],
+                'runName': run_name
             }
             adapter = mysqladapter(host=MYSQL_HOST, user=MYSQL_USER, password=MYSQL_PASSWORD, db=MYSQL_DB)
             saveForecastTimeseries(adapter, waterLevelSeriesDict[elementNo], date, time, opts)
