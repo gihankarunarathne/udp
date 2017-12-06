f19aef9b889a357678896a3d33c198aa3117ea71
Gihan Karunarathne <gckarunarathne@gmail.com>
2017-12-06 15:24:13 +0530

    Fixed: Avoid automatically Setting Timeseries start & end time
    - Assume EXTRACTFLO2DWATERLEVEL.py will use RUN_FLO2D.json configs

--- Trigger_Extract_WaterLevel.sh
+++ Trigger_Extract_WaterLevel.sh
@@ -14,8 +14,8 @@ Usage: ./Trigger_Extract_WaterLevel.sh [-d FORECAST_DATE] [-c CONFIG_FILE] [-r R
         Otherwise Default is current time.
     -b  Run forecast specified DAYS_BACK with respect to current date. Expect an integer.
         When specified -d option will be ignored.
-    -S  Base Date(Timeseries start date) of FLO2D model output in YYYY-MM-DD format. Default is same as -d option value.
-    -T  Base Time(Timeseries start time) of FLO2D model output in HH:MM:SS format. Default is same as -t option value.
+    -S  Base Date(Timeseries start date) of FLO2D model output in YYYY-MM-DD format. Need to specify in order to use.
+    -T  Base Time(Timeseries start time) of FLO2D model output in HH:MM:SS format. Need to specify in oder to use.
     -p  Path of FLO2D Model folder.
     -o  Suffix for 'water_level-<SUFFIX>' and 'water_level_grid-<SUFFIX>' output directories.
         Default is 'water_level-<YYYY-MM-DD>' and 'water_level_grid-<YYYY-MM-DD>' same as -d option value.
@@ -33,10 +33,10 @@ trimQuotes() {
 }
 
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-META_FLO2D_DIR=$ROOT_DIR/META_FLO2D
-FLO2D_DIR=$ROOT_DIR/FLO2D
+META_FLO2D_DIR=${ROOT_DIR}/META_FLO2D
+FLO2D_DIR=${ROOT_DIR}/FLO2D
 INIT_DIR=$(pwd)
-CONFIG_FILE=$ROOT_DIR/CONFIG.json
+CONFIG_FILE=${ROOT_DIR}/CONFIG.json
 
 forecast_date="`date +%Y-%m-%d`";
 forecast_time="00:00:00";
@@ -92,62 +92,63 @@ if [ "$DAYS_BACK" -gt 0 ]; then
 	forecast_date="`date +%Y-%m-%d -d "$DAYS_BACK days ago"`";
 fi
 
-if [ -z $TIMESERIES_START_DATE ]; then
-    TIMESERIES_START_DATE=$forecast_date
-fi
-if [ -z $TIMESERIES_START_TIME ]; then
-    TIMESERIES_START_TIME=$forecast_time
-fi
+# NOTE: Need to specify in order to use. Otherwise assume EXTRACTFLO2DWATERLEVEL.py will use RUN_FLO2D.json configs.
+#if [ -z ${TIMESERIES_START_DATE} ]; then
+#    TIMESERIES_START_DATE=${forecast_date}
+#fi
+#if [ -z ${TIMESERIES_START_TIME} ]; then
+#    TIMESERIES_START_TIME=${forecast_time}
+#fi
 
 # cd into bash script's root directory
-cd $ROOT_DIR
+cd ${ROOT_DIR}
 echo "At $(pwd)"
-if [ -z "$(find $CONFIG_FILE -name CONFIG.json)" ]
+if [ -z "$(find ${CONFIG_FILE} -name CONFIG.json)" ]
 then
 	echo "Unable to find $CONFIG_FILE file"
 	exit 1
 fi
 
-HOST_ADDRESS=`[[ $CUSTOM_HOST_ADDRESS != "" ]] && echo $CUSTOM_HOST_ADDRESS || echo $(trimQuotes $(cat CONFIG.json | jq '.HOST_ADDRESS'))`
-HOST_PORT=`[[ $CUSTOM_HOST_PORT != "" ]] && echo $CUSTOM_HOST_PORT || echo $(cat CONFIG.json | jq '.HOST_PORT')`
+HOST_ADDRESS=`[[ ${CUSTOM_HOST_ADDRESS} != "" ]] && echo ${CUSTOM_HOST_ADDRESS} || echo $(trimQuotes $(cat CONFIG.json | jq '.HOST_ADDRESS'))`
+HOST_PORT=`[[ ${CUSTOM_HOST_PORT} != "" ]] && echo ${CUSTOM_HOST_PORT} || echo $(cat CONFIG.json | jq '.HOST_PORT')`
 WINDOWS_HOST="$HOST_ADDRESS:$HOST_PORT"
 
 current_date_time="`date +%Y-%m-%dT%H:%M:%S`";
 
 main() {
     echo "Start at $current_date_time"
-    cp $META_FLO2D_DIR/RUN_FLO2D.json $FLO2D_DIR
+    cp ${META_FLO2D_DIR}/RUN_FLO2D.json ${FLO2D_DIR}
     # Set FLO2D model path
     FLO2D_PATH_TXT="\"FLO2D_PATH\"\t : \"$FLO2D_PATH\","
-    sed -i "/FLO2D_PATH/c\    $FLO2D_PATH_TXT" $FLO2D_DIR/RUN_FLO2D.json
+    sed -i "/FLO2D_PATH/c\    $FLO2D_PATH_TXT" ${FLO2D_DIR}/RUN_FLO2D.json
     # Set FLO2D output SUFFIX
     FLO2D_OUTPUT_SUFFIX_TXT="\"FLO2D_OUTPUT_SUFFIX\"\t : \"$FLO2D_OUTPUT_SUFFIX\","
-    sed -i "/FLO2D_OUTPUT_SUFFIX/c\    $FLO2D_OUTPUT_SUFFIX_TXT" $FLO2D_DIR/RUN_FLO2D.json
+    sed -i "/FLO2D_OUTPUT_SUFFIX/c\    $FLO2D_OUTPUT_SUFFIX_TXT" ${FLO2D_DIR}/RUN_FLO2D.json
 
     # Set Base Start Date for FLO2D (which maps to 0 step)
     TIMESERIES_START_DATE_TXT="\"TIMESERIES_START_DATE\"\t : \"$TIMESERIES_START_DATE\","
-    sed -i "/TIMESERIES_START_DATE/c\    $TIMESERIES_START_DATE_TXT" $FLO2D_DIR/RUN_FLO2D.json
+    sed -i "/TIMESERIES_START_DATE/c\    $TIMESERIES_START_DATE_TXT" ${FLO2D_DIR}/RUN_FLO2D.json
     # Set Base Start Time for FLO2D (which maps to 0 step)
     TIMESERIES_START_TIME_TXT="\"TIMESERIES_START_TIME\"\t : \"$TIMESERIES_START_TIME\","
-    sed -i "/TIMESERIES_START_TIME/c\    $TIMESERIES_START_TIME_TXT" $FLO2D_DIR/RUN_FLO2D.json
+    sed -i "/TIMESERIES_START_TIME/c\    $TIMESERIES_START_TIME_TXT" ${FLO2D_DIR}/RUN_FLO2D.json
 
     # Set Model State Date for FLO2D
     MODEL_STATE_DATE_TXT="\"MODEL_STATE_DATE\"\t : \"$forecast_date\","
-    sed -i "/MODEL_STATE_DATE/c\    $MODEL_STATE_DATE_TXT" $FLO2D_DIR/RUN_FLO2D.json
+    sed -i "/MODEL_STATE_DATE/c\    $MODEL_STATE_DATE_TXT" ${FLO2D_DIR}/RUN_FLO2D.json
     # Set Model State Time for FLO2D
     MODEL_STATE_TIME_TXT="\"MODEL_STATE_TIME\"\t : \"$forecast_time\","
-    sed -i "/MODEL_STATE_TIME/c\    $MODEL_STATE_TIME_TXT" $FLO2D_DIR/RUN_FLO2D.json
+    sed -i "/MODEL_STATE_TIME/c\    $MODEL_STATE_TIME_TXT" ${FLO2D_DIR}/RUN_FLO2D.json
 
     # Run name/tags of the Timeseries
     RUN_NAME_TXT="\"RUN_NAME\"\t : \"$RUN_NAME\","
-    sed -i "/RUN_NAME/c\    $RUN_NAME_TXT" $FLO2D_DIR/RUN_FLO2D.json
+    sed -i "/RUN_NAME/c\    $RUN_NAME_TXT" ${FLO2D_DIR}/RUN_FLO2D.json
 
     echo "Trigger FLO2D WaterLevel Extraction on Forecast Date: $forecast_date $forecast_time $TIMESERIES_START_DATE $TIMESERIES_START_TIME, Config File: $CONFIG_FILE, Root Dir: $ROOT_DIR"
 
-    curl -X POST --data-binary @./FLO2D/RUN_FLO2D.json  $WINDOWS_HOST/EXTRACT_WATERLEVEL?$forecast_date
+    curl -X POST --data-binary @./FLO2D/RUN_FLO2D.json  ${WINDOWS_HOST}/EXTRACT_WATERLEVEL?${forecast_date}
     echo "Send POST request to $WINDOWS_HOST with EXTRACT_WATERLEVEL?$forecast_date"
 
-    curl -X POST --data-binary @./FLO2D/RUN_FLO2D.json  $WINDOWS_HOST/EXTRACT_WATERLEVEL_GRID?$forecast_date
+    curl -X POST --data-binary @./FLO2D/RUN_FLO2D.json  ${WINDOWS_HOST}/EXTRACT_WATERLEVEL_GRID?${forecast_date}
     echo "Send POST request to $WINDOWS_HOST with EXTRACT_WATERLEVEL_GRID?$forecast_date"
 }
 
