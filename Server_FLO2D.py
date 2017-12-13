28ec5a26608d2ad27195342e34bc57b1c28ba87e
Gihan Karunarathne <gckarunarathne@gmail.com>
2017-12-13 16:43:57 +0530

    Reformat & Update Server to support trigger extracting Discharge

--- Server_FLO2D.py
+++ Server_FLO2D.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python
 
-import os, json, subprocess, datetime, traceback, shutil, sys
+import json
+import os
+import subprocess
+import sys
+import traceback
+from http.server import BaseHTTPRequestHandler, HTTPServer
 from os import curdir
 from os.path import join as pjoin
-from http.server import BaseHTTPRequestHandler, HTTPServer
-from sys import executable
 from subprocess import Popen
+from sys import executable
 
 root_dir = os.path.dirname(os.path.realpath(__file__))
 CONFIG = json.loads(open(pjoin(root_dir, './CONFIG.json')).read())
@@ -17,13 +21,15 @@ if 'HOST_ADDRESS' in CONFIG:
 if 'HOST_PORT' in CONFIG:
     HOST_PORT = CONFIG['HOST_PORT']
 
-FLO2D_DIR='FLO2D'
-INFLOW_DAT_FILE='INFLOW.DAT'
-RAINCELL_DAT_FILE='RAINCELL.DAT'
-RUN_FLO2D='RUN_FLO2D'
-RUN_FLO2D_FILE='RUN_FLO2D.json'
-EXTRACT_WATERLEVEL_GRID='EXTRACT_WATERLEVEL_GRID'
-EXTRACT_WATERLEVEL='EXTRACT_WATERLEVEL'
+FLO2D_DIR = 'FLO2D'
+INFLOW_DAT_FILE = 'INFLOW.DAT'
+RAINCELL_DAT_FILE = 'RAINCELL.DAT'
+RUN_FLO2D = 'RUN_FLO2D'
+RUN_FLO2D_FILE = 'RUN_FLO2D.json'
+EXTRACT_WATERLEVEL_GRID = 'EXTRACT_WATERLEVEL_GRID'
+EXTRACT_WATERLEVEL = 'EXTRACT_WATERLEVEL'
+EXTRACT_WATER_DISCHARGE = 'EXTRACT_WATER_DISCHARGE'
+
 
 # Refer: http://stackoverflow.com/a/13146494/1461060
 class StoreHandler(BaseHTTPRequestHandler):
@@ -31,7 +37,7 @@ class StoreHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path == '/'+INFLOW_DAT_FILE:
             store_path = pjoin(curdir, INFLOW_DAT_FILE)
-            with open(self.store_path) as fh:
+            with open(store_path) as fh:
                 self.send_response(200)
                 self.send_header('Content-type', 'text/json')
                 self.end_headers()
@@ -47,13 +53,13 @@ class StoreHandler(BaseHTTPRequestHandler):
             data = self.rfile.read(int(length))
             # print('DATA:', data)
 
-            FLO2D_DIR_PATH = os.path.join(curdir, FLO2D_DIR)
+            flo2d_dir_path = os.path.join(curdir, FLO2D_DIR)
             # Temporary write into FLO2D dir. Later move into FLO2D model dir while Running the model.
-            INFLOW_DAT_FILE_PATH = pjoin(FLO2D_DIR_PATH, INFLOW_DAT_FILE)
+            inflow_dat_file_path = pjoin(flo2d_dir_path, INFLOW_DAT_FILE)
 
-            inflowFile = open(INFLOW_DAT_FILE_PATH, 'w')
-            inflowFile.write(data.decode())
-            inflowFile.close()
+            inflow_file = open(inflow_dat_file_path, 'w')
+            inflow_file.write(data.decode())
+            inflow_file.close()
 
             self.send_response(200)
             self.send_header('Content-type', 'text/json')
@@ -68,13 +74,13 @@ class StoreHandler(BaseHTTPRequestHandler):
             data = self.rfile.read(int(length))
             # print('DATA:', data)
 
-            FLO2D_DIR_PATH = os.path.join(curdir, FLO2D_DIR)
+            flo2d_dir_path = os.path.join(curdir, FLO2D_DIR)
             # Temporary write into FLO2D dir. Later move into FLO2D model dir while Running the model.
-            RAINCELL_DAT_FILE_PATH = pjoin(FLO2D_DIR_PATH, RAINCELL_DAT_FILE)
+            raincell_dat_file_path = pjoin(flo2d_dir_path, RAINCELL_DAT_FILE)
 
-            raincellFile = open(RAINCELL_DAT_FILE_PATH, 'w')
-            raincellFile.write(data.decode())
-            raincellFile.close()
+            raincell_file = open(raincell_dat_file_path, 'w')
+            raincell_file.write(data.decode())
+            raincell_file.close()
 
             self.send_response(200)
             self.send_header('Content-type', 'text/json')
@@ -88,35 +94,36 @@ class StoreHandler(BaseHTTPRequestHandler):
 
             length = self.headers['content-length']
             data = self.rfile.read(int(length))
-            runConfig = json.loads(data.decode())
-            print('RUN FLO2D: RUN FLO2D options:', runConfig)
+            run_config = json.loads(data.decode())
+            print('RUN FLO2D: RUN FLO2D options:', run_config)
 
-            FLO2D_DIR_PATH = os.path.join(curdir, FLO2D_DIR)
+            flo2d_dir_path = os.path.join(curdir, FLO2D_DIR)
             # Temporary write into FLO2D dir. Later move into FLO2D model dir while Running the model.
-            RUN_FLO2D_FILE_PATH = pjoin(FLO2D_DIR_PATH, RUN_FLO2D_FILE)
-            with open(RUN_FLO2D_FILE_PATH, 'w') as runFLO2DFile:
-                json.dump(runConfig, runFLO2DFile)
+            run_flo2d_file_path = pjoin(flo2d_dir_path, RUN_FLO2D_FILE)
+            with open(run_flo2d_file_path, 'w') as runFLO2DFile:
+                json.dump(run_config, runFLO2DFile)
 
-            try :
+            try:
                 # Execute FLO2D
                 print('Execute FLO2D ...')
-                execList = [executable, pjoin(root_dir, 'Run_FLO2D.py')]
+                exec_list = [executable, pjoin(root_dir, 'Run_FLO2D.py')]
 
-                if len(date) > 0 :
-                    execList = execList + ['-d' , date]
-                if runConfig.get('FLO2D_PATH') :
-                    execList = execList + ['--model-dir' , runConfig.get('FLO2D_PATH')]
-                print('exec List:', execList)
+                if len(date) > 0:
+                    exec_list = exec_list + ['-d', date]
+                if run_config.get('FLO2D_PATH'):
+                    exec_list = exec_list + ['--model-dir', run_config.get('FLO2D_PATH')]
+                print('exec List:', exec_list)
 
                 # Popen(execList, shell=True)
-                Popen(execList, creationflags=subprocess.CREATE_NEW_CONSOLE)
+                Popen(exec_list, creationflags=subprocess.CREATE_NEW_CONSOLE)
                 # Popen(execList, stdout=sys.stdout)
-                #os.system('python Run_FLO2D.py'+ date)
+                # os.system('python Run_FLO2D.py'+ date)
                 self.send_response(200)
                 self.send_header('Content-type', 'text/json')
                 self.end_headers()
                 return
-            except Exception as e :
+            except Exception as e:
+                print(e)
                 traceback.print_exc()
                 self.send_response(500)
                 self.end_headers()
@@ -128,41 +135,42 @@ class StoreHandler(BaseHTTPRequestHandler):
 
             length = self.headers['content-length']
             data = self.rfile.read(int(length))
-            runConfig = json.loads(data.decode())
-            print('WATER_LEVEL_GRID: RUN FLO2D options:', runConfig)
+            run_config = json.loads(data.decode())
+            print('WATER_LEVEL_GRID: RUN FLO2D options:', run_config)
 
-            try :
+            try:
                 # Execute WATERLEVEL GRID extraction
                 print('Execute WATERLEVEL GRID extraction ...')
-                execList = ["powershell.exe", pjoin(root_dir, '.\CopyWaterLevelGridToCMS.ps1')]
-                if len(date) > 0 :
-                    execList = execList + ['-date' , date]
-                if runConfig.get('MODEL_STATE_TIME') :
-                    execList = execList + ['-time' , runConfig.get('MODEL_STATE_TIME')]
+                exec_list = ["powershell.exe", pjoin(root_dir, '.\CopyWaterLevelGridToCMS.ps1')]
+                if len(date) > 0:
+                    exec_list = exec_list + ['-date', date]
+                if run_config.get('MODEL_STATE_TIME'):
+                    exec_list = exec_list + ['-time', run_config.get('MODEL_STATE_TIME')]
 
-                if runConfig.get('TIMESERIES_START_DATE') :
-                    execList = execList + ['-start_date' , runConfig.get('TIMESERIES_START_DATE')]
-                if runConfig.get('TIMESERIES_START_TIME') :
-                    execList = execList + ['-start_time' , runConfig.get('TIMESERIES_START_TIME')]
+                if run_config.get('TIMESERIES_START_DATE'):
+                    exec_list = exec_list + ['-start_date', run_config.get('TIMESERIES_START_DATE')]
+                if run_config.get('TIMESERIES_START_TIME'):
+                    exec_list = exec_list + ['-start_time', run_config.get('TIMESERIES_START_TIME')]
 
-                if runConfig.get('FLO2D_PATH') :
-                    execList = execList + ['-path' , runConfig.get('FLO2D_PATH')]
-                if runConfig.get('FLO2D_OUTPUT_SUFFIX') :
-                    execList = execList + ['-out' , runConfig.get('FLO2D_OUTPUT_SUFFIX')]
+                if run_config.get('FLO2D_PATH'):
+                    exec_list = exec_list + ['-path', run_config.get('FLO2D_PATH')]
+                if run_config.get('FLO2D_OUTPUT_SUFFIX'):
+                    exec_list = exec_list + ['-out', run_config.get('FLO2D_OUTPUT_SUFFIX')]
 
-                if runConfig.get('RUN_NAME') :
-                    execList = execList + ['-name' , runConfig.get('RUN_NAME')]
+                if run_config.get('RUN_NAME'):
+                    exec_list = exec_list + ['-name', run_config.get('RUN_NAME')]
 
-                print('exec List:', execList)
+                print('exec List:', exec_list)
 
-                Popen(execList, stdout=sys.stdout)
-                #os.system('python CopyWaterLevelGridToCMS.ps1 '+ date)
+                Popen(exec_list, stdout=sys.stdout)
+                # os.system('python CopyWaterLevelGridToCMS.ps1 '+ date)
 
                 self.send_response(200)
                 self.send_header('Content-type', 'text/json')
                 self.end_headers()
                 return
-            except Exception as e :
+            except Exception as e:
+                print(e)
                 traceback.print_exc()
                 self.send_response(500)
                 self.end_headers()
@@ -174,43 +182,91 @@ class StoreHandler(BaseHTTPRequestHandler):
 
             length = self.headers['content-length']
             data = self.rfile.read(int(length))
-            runConfig = json.loads(data.decode())
-            print('WATER_LEVEL: RUN FLO2D options:', runConfig)
+            run_config = json.loads(data.decode())
+            print('WATER_LEVEL: RUN FLO2D options:', run_config)
 
-            try :
+            try:
                 # Execute WATERLEVEL extraction
                 print('Execute WATERLEVEL extraction ...')
-                execList = ["powershell.exe", pjoin(root_dir, '.\CopyWaterLevelToCMS.ps1')]
-                if len(date) > 0 :
-                    execList = execList + ['-date' , date]
-                if runConfig.get('MODEL_STATE_TIME') :
-                    execList = execList + ['-time' , runConfig.get('MODEL_STATE_TIME')]
+                exec_list = ["powershell.exe", pjoin(root_dir, '.\CopyWaterLevelToCMS.ps1')]
+                if len(date) > 0:
+                    exec_list = exec_list + ['-date', date]
+                if run_config.get('MODEL_STATE_TIME'):
+                    exec_list = exec_list + ['-time', run_config.get('MODEL_STATE_TIME')]
 
-                if runConfig.get('TIMESERIES_START_DATE') :
-                    execList = execList + ['-start_date' , runConfig.get('TIMESERIES_START_DATE')]
-                if runConfig.get('TIMESERIES_START_TIME') :
-                    execList = execList + ['-start_time' , runConfig.get('TIMESERIES_START_TIME')]
+                if run_config.get('TIMESERIES_START_DATE'):
+                    exec_list = exec_list + ['-start_date', run_config.get('TIMESERIES_START_DATE')]
+                if run_config.get('TIMESERIES_START_TIME'):
+                    exec_list = exec_list + ['-start_time', run_config.get('TIMESERIES_START_TIME')]
 
-                if runConfig.get('FLO2D_PATH') :
-                    execList = execList + ['-path' , runConfig.get('FLO2D_PATH')]
-                if runConfig.get('FLO2D_OUTPUT_SUFFIX') :
-                    execList = execList + ['-out' , runConfig.get('FLO2D_OUTPUT_SUFFIX')]
+                if run_config.get('FLO2D_PATH'):
+                    exec_list = exec_list + ['-path', run_config.get('FLO2D_PATH')]
+                if run_config.get('FLO2D_OUTPUT_SUFFIX'):
+                    exec_list = exec_list + ['-out', run_config.get('FLO2D_OUTPUT_SUFFIX')]
 
-                if runConfig.get('RUN_NAME') :
-                    execList = execList + ['-name' , runConfig.get('RUN_NAME')]
+                if run_config.get('RUN_NAME'):
+                    exec_list = exec_list + ['-name', run_config.get('RUN_NAME')]
                 # TODO: Handle passing forceInsert
-                execList = execList + ['-forceInsert', "True"]
+                exec_list = exec_list + ['-forceInsert', "True"]
 
-                print('exec List:', execList)
+                print('exec List:', exec_list)
 
-                Popen(execList, stdout=sys.stdout)
-                #os.system('python CopyWaterLevelToCMS.ps1 '+ date)
+                Popen(exec_list, stdout=sys.stdout)
+                # os.system('python CopyWaterLevelToCMS.ps1 '+ date)
 
                 self.send_response(200)
                 self.send_header('Content-type', 'text/json')
                 self.end_headers()
                 return
-            except Exception as e :
+            except Exception as e:
+                print(e)
+                traceback.print_exc()
+                self.send_response(500)
+                self.end_headers()
+
+        # Run FLO2D WATER DISCHARGE extraction from HYCHAN.OUT
+        elif self.path.startswith('/' + EXTRACT_WATER_DISCHARGE):
+            date = self.path[len('/' + EXTRACT_WATER_DISCHARGE) + 1:]
+            print('POST request on ', self.path, date)
+
+            length = self.headers['content-length']
+            data = self.rfile.read(int(length))
+            run_config = json.loads(data.decode())
+            print('WATER_DISCHARGE: RUN FLO2D options:', run_config)
+
+            try:
+                # Execute WATER DISCHARGE extraction
+                print('Execute WATER DISCHARGE extraction ...')
+                exec_list = ["powershell.exe", pjoin(root_dir, '.\CopyFLO2DDischargeToCMS.ps1')]
+                if len(date) > 0:
+                    exec_list = exec_list + ['-date', date]
+                if run_config.get('MODEL_STATE_TIME'):
+                    exec_list = exec_list + ['-time', run_config.get('MODEL_STATE_TIME')]
+
+                if run_config.get('TIMESERIES_START_DATE'):
+                    exec_list = exec_list + ['-start_date', run_config.get('TIMESERIES_START_DATE')]
+                if run_config.get('TIMESERIES_START_TIME'):
+                    exec_list = exec_list + ['-start_time', run_config.get('TIMESERIES_START_TIME')]
+
+                if run_config.get('FLO2D_PATH'):
+                    exec_list = exec_list + ['-path', run_config.get('FLO2D_PATH')]
+                if run_config.get('FLO2D_OUTPUT_SUFFIX'):
+                    exec_list = exec_list + ['-out', run_config.get('FLO2D_OUTPUT_SUFFIX')]
+
+                if run_config.get('RUN_NAME'):
+                    exec_list = exec_list + ['-name', run_config.get('RUN_NAME')]
+                # TODO: Handle passing forceInsert
+                exec_list = exec_list + ['-forceInsert', "True"]
+
+                print('exec List:', exec_list)
+                Popen(exec_list, stdout=sys.stdout)
+
+                self.send_response(200)
+                self.send_header('Content-type', 'text/json')
+                self.end_headers()
+                return
+            except Exception as e:
+                print(e)
                 traceback.print_exc()
                 self.send_response(500)
                 self.end_headers()
