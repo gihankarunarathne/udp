31a9dce0ab48c3ad26eb1f4b8179fad75a10b114
Gihan Karunarathne <gckarunarathne@gmail.com>
2017-12-20 00:19:23 +0530

    Reformat Update_HECHMS script

--- Update_HECHMS.py
+++ Update_HECHMS.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
 
-import sys, traceback, csv, json, datetime, collections, getopt, re, os
+import collections
+import csv
+import datetime
+import getopt
+import json
+import os
+import re
+import sys
+import traceback
 
 DSSDateTime = collections.namedtuple('DSSDateTime', ['dateTime', 'date', 'time'])
 
-def usage() :
-    usageText = """
+
+def usage():
+    usage_text = """
 Usage: ./Update_HECHMS.py [-d date] [-h -i] [-s sInterval] [-c cInterval] 
 
 -h  --help          Show usage
@@ -20,48 +29,35 @@ Usage: ./Update_HECHMS.py [-d date] [-h -i] [-s sInterval] [-c cInterval]
     --hec-hms-model-dir Path of HEC_HMS_MODEL_DIR directory. 
                         Otherwise using the `HEC_HMS_MODEL_DIR` from CONFIG.json
 """
-    print(usageText)
+    print(usage_text)
 
-def getDSSDateTime(dateTime) :
-    value = dateTime.strftime('%Y:%m:%d %H:%M:%S')
-    s = value.split(' ')
-    d = s[0].split(':')
-    t = s[1].split(':')
-    if(int(t[0]) == 0) : # In case of 00:00, mention it as 24:00 of previous day
-        dateTimeTmp = dateTime - datetime.timedelta(hours=1)
-        date = dateTimeTmp.strftime('%d %B %Y')
-        time = dateTimeTmp.strftime('%H:%M')
-        time = time.split(':')
-        time[0] = '24'
-        time = ':'.join(time)
-    else :
-        date = dateTime.strftime('%d %B %Y')
-        time = dateTime.strftime('%H:%M')
 
-    # Removed DSS formating with HEC-HMS upgrading from 3.5 to 4.1
-    date = dateTime.strftime('%d %B %Y')
-    time = dateTime.strftime('%H:%M')
+def get_dss_date_time(date_time):
+    # Removed DSS formatting with HEC-HMS upgrading from 3.5 to 4.1
+    my_date = date_time.strftime('%d %B %Y')
+    my_time = date_time.strftime('%H:%M')
 
     return DSSDateTime(
-        dateTime  = date + ' ' + time,
-        date      = date,
-        time      = time
+        dateTime=my_date + ' ' + my_time,
+        date=my_date,
+        time=my_time
     )
 
-try :
+
+try:
     CONFIG = json.loads(open('CONFIG.json').read())
     # print('Config :: ', CONFIG)
 
-    NUM_METADATA_LINES = 3;
+    NUM_METADATA_LINES = 3
     HEC_HMS_MODEL_DIR = './2008_2_Events'
     HEC_HMS_CONTROL = './2008_2_Events/Control_1.control'
     HEC_HMS_RUN = './2008_2_Events/2008_2_Events.run'
     HEC_HMS_GAGE = './2008_2_Events/2008_2_Events.gage'
     RAIN_CSV_FILE = 'DailyRain.csv'
-    TIME_INTERVAL = 60 # In minutes
+    TIME_INTERVAL = 60  # In minutes
     OUTPUT_DIR = './OUTPUT'
-    STATE_INTERVAL = 1 * 24 * 60 # In minutes (1 day)
-    CONTROL_INTERVAL = 8 * 24 * 60 # In minutes (8 day)
+    STATE_INTERVAL = 1 * 24 * 60  # In minutes (1 day)
+    CONTROL_INTERVAL = 8 * 24 * 60  # In minutes (8 day)
 
     if 'HEC_HMS_MODEL_DIR' in CONFIG :
         HEC_HMS_MODEL_DIR = CONFIG['HEC_HMS_MODEL_DIR']
@@ -100,9 +96,9 @@ try :
             date = arg
         elif opt in ("-t", "--time"):
             time = arg
-        elif opt in ("--start-date"):
+        elif opt in "--start-date":
             startDateTS = arg
-        elif opt in ("--start-time"):
+        elif opt in "--start-time":
             startTimeTS = arg
         elif opt in ("-i", "--init"):
             initState = True
@@ -112,46 +108,48 @@ try :
             CONTROL_INTERVAL = int(arg)
         elif opt in ("-T", "--tag"):
             tag = arg
-        elif opt in ("--hec-hms-model-dir"):
+        elif opt in "--hec-hms-model-dir":
             HEC_HMS_MODEL_DIR = arg
 
     # Replace CONFIG.json variables
-    if re.match('^\$\{(HEC_HMS_MODEL_DIR)\}', HEC_HMS_CONTROL) :
+    if re.match('^\$\{(HEC_HMS_MODEL_DIR)\}', HEC_HMS_CONTROL):
         HEC_HMS_CONTROL = re.sub('^\$\{(HEC_HMS_MODEL_DIR)\}', '', HEC_HMS_CONTROL).strip("/\\")
         HEC_HMS_CONTROL = os.path.join(HEC_HMS_MODEL_DIR, HEC_HMS_CONTROL)
         print('Set HEC_HMS_CONTROL=', HEC_HMS_CONTROL)
 
-    if re.match('^\$\{(HEC_HMS_MODEL_DIR)\}', HEC_HMS_RUN) :
+    if re.match('^\$\{(HEC_HMS_MODEL_DIR)\}', HEC_HMS_RUN):
         HEC_HMS_RUN = re.sub('^\$\{(HEC_HMS_MODEL_DIR)\}', '', HEC_HMS_RUN).strip("/\\")
         HEC_HMS_RUN = os.path.join(HEC_HMS_MODEL_DIR, HEC_HMS_RUN)
         print('Set HEC_HMS_RUN=', HEC_HMS_RUN)
 
-    if re.match('^\$\{(HEC_HMS_MODEL_DIR)\}', HEC_HMS_GAGE) :
+    if re.match('^\$\{(HEC_HMS_MODEL_DIR)\}', HEC_HMS_GAGE):
         HEC_HMS_GAGE = re.sub('^\$\{(HEC_HMS_MODEL_DIR)\}', '', HEC_HMS_GAGE).strip("/\\")
         HEC_HMS_GAGE = os.path.join(HEC_HMS_MODEL_DIR, HEC_HMS_GAGE)
         print('Set HEC_HMS_GAGE=', HEC_HMS_GAGE)
 
     # Default run for current day
     modelState = datetime.datetime.now()
-    if date :
+    if date:
         modelState = datetime.datetime.strptime(date, '%Y-%m-%d')
     date = modelState.strftime("%Y-%m-%d")
-    if time :
+    if time:
         modelState = datetime.datetime.strptime('%s %s' % (date, time), '%Y-%m-%d %H:%M:%S')
     time = modelState.strftime("%H:%M:%S")
 
     startDateTimeTS = datetime.datetime.now()
-    if startDateTS :
+    if startDateTS:
         startDateTimeTS = datetime.datetime.strptime(startDateTS, '%Y-%m-%d')
-    else :
+    else:
         startDateTimeTS = datetime.datetime.strptime(date, '%Y-%m-%d')
     startDateTS = startDateTimeTS.strftime("%Y-%m-%d")
 
-    if startTimeTS :
+    if startTimeTS:
         startDateTimeTS = datetime.datetime.strptime('%s %s' % (startDateTS, startTimeTS), '%Y-%m-%d %H:%M:%S')
     startTimeTS = startDateTimeTS.strftime("%H:%M:%S")
 
     print('Update_HECHMS startTime:', datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), ', initState:', initState)
+    print(' Update_HECHMS run for', date, '@', time, tag)
+    print(' With Custom Time Series starting', startDateTS, '@', startTimeTS)
     print('Control Interval:', CONTROL_INTERVAL/60, 'hours')
     # Extract Start and End times
     fileName = RAIN_CSV_FILE.rsplit('.', 1)
@@ -165,8 +163,8 @@ try :
     startDateTime = datetime.datetime.strptime(csvList[NUM_METADATA_LINES][0], '%Y-%m-%d %H:%M:%S')
     endDateTime = datetime.datetime.strptime(csvList[-1][0], '%Y-%m-%d %H:%M:%S')
 
-    startDateTimeDSS = getDSSDateTime(startDateTime)
-    endDateTimeDSS = getDSSDateTime(endDateTime)
+    startDateTimeDSS = get_dss_date_time(startDateTime)
+    endDateTimeDSS = get_dss_date_time(endDateTime)
     startDate = startDateTimeDSS.date
     startTime = startDateTimeDSS.time
     endDate = endDateTimeDSS.date
@@ -174,7 +172,7 @@ try :
 
     # TODO : Increase Control Interval with gap between startDateTimeTS and modelState
     controlEndDateTime = startDateTime + datetime.timedelta(minutes=CONTROL_INTERVAL)
-    controlEndDateTimeDSS = getDSSDateTime(controlEndDateTime)
+    controlEndDateTimeDSS = get_dss_date_time(controlEndDateTime)
     controlEndDate = controlEndDateTimeDSS.date
     controlEndTime = controlEndDateTimeDSS.time
 
@@ -186,28 +184,28 @@ try :
     controlFile.close()
 
     controlFile = open(HEC_HMS_CONTROL, 'w')
-    for line in controlData :
-        if 'Start Date:' in line :
+    for line in controlData:
+        if 'Start Date:' in line:
             s = line[:line.rfind('Start Date:')+11]
             s += ' ' + startDate
             controlFile.write(s + '\n')
-        elif 'Start Time:' in line :
+        elif 'Start Time:' in line:
             s = line[:line.rfind('Start Time:')+11]
             s += ' ' + startTime
             controlFile.write(s + '\n')
-        elif 'End Date:' in line :
+        elif 'End Date:' in line:
             s = line[:line.rfind('End Date:')+9]
             s += ' ' + controlEndDate
             controlFile.write(s + '\n')
-        elif 'End Time:' in line :
+        elif 'End Time:' in line:
             s = line[:line.rfind('End Time:')+9]
             s += ' ' + controlEndTime
             controlFile.write(s + '\n')
-        elif 'Time Interval:' in line :
+        elif 'Time Interval:' in line:
             s = line[:line.rfind('Time Interval:')+14]
             s += ' ' + str(TIME_INTERVAL)
             controlFile.write(s + '\n')
-        else :
+        else:
             controlFile.write(line)
 
     #############################################
@@ -218,34 +216,34 @@ try :
     runFile.close()
 
     runFile = open(HEC_HMS_RUN, 'w')
-    for line in runData :
-        if 'Control:' in line :
+    for line in runData:
+        if 'Control:' in line:
             runFile.write(line)
             indent = line[:line.rfind('Control:')]
 
             # TODO: Handle for Hour mode
             saveStateDateTime = startDateTime + datetime.timedelta(minutes=STATE_INTERVAL)
-            saveStateDateTimeDSS = getDSSDateTime(saveStateDateTime)
+            saveStateDateTimeDSS = get_dss_date_time(saveStateDateTime)
             startStateDateTime = startDateTime - datetime.timedelta(minutes=STATE_INTERVAL)
             line1 = indent + 'Save State Name: State_' + startDateTime.strftime('%Y_%m_%d') + '_To_' + saveStateDateTime.strftime('%Y_%m_%d')
             line2 = indent + 'Save State Date: ' + saveStateDateTimeDSS.date
             line3 = indent + 'Save State Time: ' + saveStateDateTimeDSS.time
             runFile.write(line1 + '\n'); runFile.write(line2 + '\n'); runFile.write(line3 + '\n')
 
-            if not initState :
+            if not initState:
                 line4 = indent + 'Start State Name: State_' + startStateDateTime.strftime('%Y_%m_%d') + '_To_' + startDateTime.strftime('%Y_%m_%d')
                 runFile.write(line4 + '\n')
 
         # Skip Writing these lines
-        elif 'Save State At End of Run:' in line :
+        elif 'Save State At End of Run:' in line:
             continue
-        elif 'Save State Name:' in line :
+        elif 'Save State Name:' in line:
             continue
-        elif 'Save State Date:' in line :
+        elif 'Save State Date:' in line:
             continue
-        elif 'Save State Time:' in line :
+        elif 'Save State Time:' in line:
             continue
-        elif 'Start State Name:' in line :
+        elif 'Start State Name:' in line:
             continue
         else :
             runFile.write(line)
@@ -258,21 +256,22 @@ try :
     gageFile.close()
 
     gageFile = open(HEC_HMS_GAGE, 'w')
-    for line in gageData :
-        if 'Start Time:' in line :
+    for line in gageData:
+        if 'Start Time:' in line:
             s = line[:line.rfind('Start Time:')+11]
             s += ' ' + startDate + ', ' + startTime
             gageFile.write(s + '\n')
-        elif 'End Time:' in line :
+        elif 'End Time:' in line:
             s = line[:line.rfind('End Time:')+9]
             s += ' ' + endDate + ', ' + endTime
             gageFile.write(s + '\n')
-        else :
+        else:
             gageFile.write(line)
 
 
-except Exception as e :
+except Exception as e:
     traceback.print_exc()
+    print(e)
 finally:
     controlFile.close()
-    print('Updated HEC-HMS Control file ', HEC_HMS_CONTROL, HEC_HMS_RUN, HEC_HMS_GAGE)
+    print('Updated HEC-HMS Control file ')
